158c5e0496179f29be06bd8527a7b5153713bd79
saraswathy <saraswathychandra88@gmail.com>
2023-03-03 12:03:45 +0100

    latest commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/service/order-details.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+ 
+  constructor(private service:OrderDetailsService){}
+  foodItem:any;
+  ngOnInit():void{
+    this.foodItem=this.service.foodDetails;
+  }
 
 }
--- src/app/service/order-details.service.ts
+++ src/app/service/order-details.service.ts
@@ -10,6 +10,68 @@ export class OrderDetailsService {
   // foodDetails
   
   foodDetails=[
+    {
+      id:1,
+      foodName:"Pizza",
+      foodDetails:"mushroom with cheese",
+      foodPrice:10,
+      foodImg:"../../../assets/img/pizzaHomeimg.jpg"
+      
+    },
+    {
+      id:2,
+      foodName:"Burger",
+      foodDetails:"chicken Burger",
+      foodPrice:20,
+      foodImg:"../../../assets/img/burgernew.jpg"
+    },
+    {
+      id:3,
+      foodName:"French Toast",
+      foodDetails:"Authentic french toast",
+      foodPrice:6,
+      foodImg:"../../../assets/img/frenchtost.jpg"
+    },
+    {
+      id:4,
+      foodName:"Pasta",
+      foodDetails:"Italian Pasta",
+      foodPrice:16,
+      foodImg:"../../../assets/img/pasta.jpg"
+    },
+    {
+      id:5,
+      foodName:"salad",
+      foodDetails:"Fresh salad",
+      foodPrice:7,
+      foodImg:"../../../assets/img/salad.jpg"
+    },
+    {
+      id:6,
+      foodName:"Pasta",
+      foodDetails:"Italian Pasta",
+      foodPrice:16,
+      foodImg:"../../../assets/img/pasta.jpg"
+    },
+    {
+      id:7,
+      foodName:"French Toast",
+      foodDetails:"Authentic french toast",
+      foodPrice:6,
+      foodImg:"../../../assets/img/frenchtost.jpg"
+    },
+    {
+      id:8,
+      foodName:"Burger",
+      foodDetails:"chicken Burger",
+      foodPrice:20,
+      foodImg:"../../../assets/img/burgernew.jpg"
+    },
+
+
+
+
+    
 
   ]
 }
